3e42b7134e1e1b556ae7c49650e690d1b588a926
EXPYH <dublineryh@gmail.com>
2018-04-12 14:43:14 +0900

    Add first algo

---
+++ quicksort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define MAX_NUM 100
+
+int numbers[MAX_NUM];
+
+int num;
+
+void quickSort(int first, int last){
+    int pivot;
+    int i;
+    int j;
+    int temp;
+
+    if (first < last){
+        pivot = first;
+        i = first;
+        j = last;
+
+        while (i < j){
+            while (numbers[i] <= numbers[pivot] && i < last)
+                i++;
+            while (numbers[j] > numbers[pivot])
+                j--;
+            // number[i] >= number[pivot] > number [j]
+            if (i < j){
+                //make number[i] < number[pivot] <= number[j]
+                temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+
+        // in this positin , i >= j and number[i] > number[pivot] >= number[j]
+
+        temp = numbers[pivot];
+        numbers[pivot] = numbers[j];
+        numbers[j] = temp;
+
+        quickSort(first, j - 1);
+        quickSort(j + 1, last);
+    }
+}
