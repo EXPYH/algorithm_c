8fe885d6e91eb5a24f297b77963f5daedf9856d1
EXPYH <dublineryh@gmail.com>
2018-04-17 22:38:54 +0900

    Add files via upload
    Basic functions of character array using pointer.

---
+++ string_functions.c
@@ -0,0 +1,19 @@
+int strlen(char* s) {
+	char *p = s;
+	while (*p != '\0')
+		p++;
+	return p - s;
+}
+
+void strcpy(char *s, char* t) {
+	//when *t is '\0', this while statement becomes false
+	while (*s++ = *t++)
+		;
+}
+
+int strcmp(char *s, char* t) {
+	for (; *s == *t; s++, t++)
+		if (*s == '\0')
+			return 0;
+	return *s - *t;
+}
